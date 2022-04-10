3ca7759a579db1bd4bdbbfc9f3e8817dc4817a99
Hawzen <hawzen@users.noreply.github.com>
2022-04-10 18:12:22 +0300

    Add stringify

--- exam_backend_django/models.py
+++ exam_backend_django/models.py
@@ -15,6 +15,9 @@ class Student(models.Model):
     date_registered = models.DateTimeField(auto_now_add=True)    
     exams = models.ManyToManyField("Exam", through="Student_on_Exam")
 
+    def __str__(self):
+        return f"{self.username=}\n\t{self.student_id=}\n\t{self.date_registered=}\n\t{self.exams.count()=}"
+
 class Exam(models.Model):
     id = models.UUIDField(
         primary_key=True,
@@ -35,9 +38,16 @@ class Exam(models.Model):
     open_time = models.DateTimeField()
     close_time = models.DateTimeField()
 
+    def __str__(self):
+        return f"{self.exam_name=}\n\t{self.course_name=}\n\t{self.course_id=}\n\t{self.num_questions=}\n\t{self.graded=}\n\t{self.total_marks=}" + \
+            f"\n\t{self.date_registered=}\n\t{self.open_time=}\n\t{self.close_time=}"
+
 class Student_on_Exam(models.Model):
     student = models.ForeignKey(Student, on_delete=models.CASCADE)
     exam = models.ForeignKey(Exam, on_delete=models.CASCADE)
     student_marks = models.DecimalField(max_digits=11, decimal_places=5)
     is_practice = models.BooleanField(default=False, )
     date_student_finished = models.DateTimeField()
+    
+    def __str__(self):
+        return f"{self.student.username=}\n\t{self.exam.exam_name=}\n\t{self.student_marks=}\n\t{self.is_practice=}\n\t{self.date_student_finished=}"
