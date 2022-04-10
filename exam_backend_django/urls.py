2214803b7dbcda7b1bf33c530306b6d3480d74b4
Hawzen <hawzen@users.noreply.github.com>
2022-04-10 19:47:26 +0300

    Refactor code

--- exam_backend_django/urls.py
+++ exam_backend_django/urls.py
@@ -20,5 +20,6 @@ from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('user/login', views.login_view),
-    path('user/register', views.register_view)
+    path('user/register', views.register_view),
+    path('user/logout', views.logout_view),
 ]
--- exam_backend_django/views.py
+++ exam_backend_django/views.py
@@ -1,44 +1,48 @@
 import json
+from functools import wraps
 
 from django.contrib.auth import authenticate, login, logout
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from django.contrib.auth.decorators import login_required
+from django.views.decorators.http import require_http_methods
 
 from django.contrib.auth.models import User
 
 from . import models
 
-# login_required
+def require_params(*register_params):
+    def decorator(function):
+        def wrapper(request, *args, **kwargs):
+            data = json.loads(request.body)
+            if not any(param in data for param in register_params):
+                return JsonResponse({'status':'failed', "message": "wrong parameters, or wrong format."})
+            return function(request, *args, **kwargs)
+        return wrapper
+    return decorator
 
 @csrf_exempt
+@require_http_methods(["POST"])
+@require_params("student_id", "password", "username")
 def register_view(request):
-    register_params = ("student_id", "password", "username")
     data = json.loads(request.body)
-    if request.method != 'POST':
-        return JsonResponse({'status':'failed', "message": "The request header isn't a POST request."})
-    if not any(param in data for param in register_params):
-        return JsonResponse({'status':'failed', "message": "wrong parameters, or wrong format."})
     user = User.objects.create_user(username=data['student_id'], password=data['password'])
     models.Student.objects.create(student_id=data['student_id'], username=data['username'], user=user)
     return JsonResponse({'status':'success'})
     
-
 @csrf_exempt
+@require_http_methods(["POST"])
+@require_params("student_id", "password")
 def login_view(request):
     data = json.loads(request.body)
-    if request.method != 'POST':
-        return JsonResponse({'status':'failed', "message": "The request header isn't a POST request."})
-    if "student_id" not in data or "password" not in data:
-        return JsonResponse({'status':'failed', "message": "wrong parameters, or wrong format."})
     user = authenticate(request, username=data['student_id'], password=data['password'])
     if user is not None:
         login(request, user)
-        student = User.objects.get(username=user.get_username())
         return JsonResponse({'status':'success'})
     else:
         return JsonResponse({'status':'failed', "message": "Incorrect login details"})
 
-@login_required
+@require_http_methods(["POST"])
 def logout_view(request):
     logout(request)
+    return JsonResponse({'status':'success'})
