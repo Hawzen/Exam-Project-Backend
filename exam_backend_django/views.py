f248e4f24aa15f46fe84c81ba74bf8150fb195dc
Hawzen <hawzen@users.noreply.github.com>
2022-04-10 20:00:22 +0300

    Add login_required decorator

--- exam_backend_django/views.py
+++ exam_backend_django/views.py
@@ -2,24 +2,33 @@ import json
 from functools import wraps
 
 from django.contrib.auth import authenticate, login, logout
-from django.http import JsonResponse
+from django.http import JsonResponse, HttpResponse
 from django.views.decorators.csrf import csrf_exempt
-from django.contrib.auth.decorators import login_required
 from django.views.decorators.http import require_http_methods
 
 from django.contrib.auth.models import User
 
 from . import models
 
+# Decorators 
 def require_params(*register_params):
     def decorator(function):
         def wrapper(request, *args, **kwargs):
             data = json.loads(request.body)
             if not any(param in data for param in register_params):
-                return JsonResponse({'status':'failed', "message": "wrong parameters, or wrong format."})
+                return JsonResponse({'status':'failed', "message": "wrong parameters, or wrong format."}, status=400)
             return function(request, *args, **kwargs)
         return wrapper
     return decorator
+
+def login_required(function):
+    def wrapper(request, *args, **kwargs):
+        if not request.user.is_authenticated:
+            return JsonResponse({"message": "user not authenticated."}, status=401)
+        return function(request, *args, **kwargs)
+    return wrapper
+
+# Views
 
 @csrf_exempt
 @require_http_methods(["POST"])
@@ -28,7 +37,7 @@ def register_view(request):
     data = json.loads(request.body)
     user = User.objects.create_user(username=data['student_id'], password=data['password'])
     models.Student.objects.create(student_id=data['student_id'], username=data['username'], user=user)
-    return JsonResponse({'status':'success'})
+    return JsonResponse({"message": "success"}, status=200)
     
 @csrf_exempt
 @require_http_methods(["POST"])
@@ -38,11 +47,14 @@ def login_view(request):
     user = authenticate(request, username=data['student_id'], password=data['password'])
     if user is not None:
         login(request, user)
-        return JsonResponse({'status':'success'})
+        return JsonResponse({"message": "success"}, status=200)
     else:
-        return JsonResponse({'status':'failed', "message": "Incorrect login details"})
+        return JsonResponse({'status':'failed', "message": "Incorrect login details"}, status=401)
 
+@csrf_exempt
+@login_required
 @require_http_methods(["POST"])
 def logout_view(request):
     logout(request)
-    return JsonResponse({'status':'success'})
+    return JsonResponse({"message": "success"}, status=200)
+
