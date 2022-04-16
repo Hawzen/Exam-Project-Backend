57c5361b2c7cd1cfef98e5d9081a3b0932a70dc3
Hawzen <hawzen@users.noreply.github.com>
2022-04-16 05:33:49 +0300

    Schema draft #1

--- exam_backend_django/models.py
+++ exam_backend_django/models.py
@@ -2,7 +2,6 @@ from django.db import models
 from django.contrib.auth.models import User
 import uuid
 
-
 class Student(models.Model):
     id = models.UUIDField(
         primary_key=True,
@@ -12,8 +11,9 @@ class Student(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, unique=True)
     username = models.CharField(max_length=256)
     student_id = models.CharField(max_length=256, unique=True)
-    date_registered = models.DateTimeField(auto_now_add=True)    
+    date_registered = models.DateTimeField(auto_now_add=True)
     exams = models.ManyToManyField("Exam", through="Student_on_Exam")
+    authority = models.BooleanField(default=False, help_text="Makes and edits exams")
 
     def __str__(self):
         return f"""
@@ -29,16 +29,15 @@ class Exam(models.Model):
         default=uuid.uuid4,
         editable=False
     )
+    creator = models.ForeignKey(Student, on_delete=models.SET_NULL, null=True, default=None)
     exam_name = models.CharField(max_length=256, unique=True)
     course_name = models.CharField(max_length=256)
     course_id = models.CharField(max_length=256)
-    
-    exam_content = models.TextField()
-    num_questions = models.PositiveIntegerField(blank=True, null=True)
-    
-    graded = models.BooleanField(default=True)
+    description = models.TextField(default="")
+    num_questions = models.PositiveIntegerField()
+    is_practice = models.BooleanField(default=False)
+    practice_exam = models.OneToOneField("self", on_delete=models.SET_NULL, null=True)
     total_marks = models.DecimalField(max_digits=10, decimal_places=5, null=True, blank=True)
-    
     date_registered = models.DateTimeField(auto_now_add=True)
     open_time = models.DateTimeField()
     close_time = models.DateTimeField()
@@ -62,12 +61,15 @@ class Question(models.Model):
         default=uuid.uuid4,
         editable=False
     )
+    # image = models.ImageField
     exam = models.ForeignKey(Exam, on_delete=models.SET_NULL, null=True)
     description = models.TextField()
+    question_index = models.SmallIntegerField()
     class QuestionType(models.TextChoices):
         YES_NO = "YN", "Yes\\No"
         CHOOSE_ONE = "CO", "Choose one"
         CHOOSE_MANY = "CM", "Choose many"
+        SHORT_ANSWER = "SA", "Short answer"
     qtype = models.CharField(
         max_length=3,
         choices= QuestionType.choices,
@@ -75,20 +77,38 @@ class Question(models.Model):
     )
     choices = models.JSONField()
     answers = models.JSONField()
+    hint = models.TextField()
+    """
+    Examples:
+    YES_NO:
+        description: "Is the earth flat"
+        choices = {}
+        answers = {"0": 0}
+    CHOOSE_ONE:
+        description: "Which one of the following items isn't a fruit"
+        choices = {"0": ["Apple", "Orange", "Table"]}
+        answers = {"0": 2}
+    CHOOSE_MANY:
+        description: "Which one of the following items is a fruit"
+        choices = {"0": ["Apple", "Orange", "Table"]}
+        answers = {"0": [0, 1]}
+    SHORT_ANSWER:
+        description: "What is the language that dominates the web"
+        choices = {}
+        answers = {"0": "Javascript"}
+    """
 
 def empty_dict():
     return {}
-
 
 class Student_on_Exam(models.Model):
     student = models.ForeignKey(Student, on_delete=models.CASCADE)
     exam = models.ForeignKey(Exam, on_delete=models.CASCADE)
     student_answers = models.JSONField(default=empty_dict)
     student_marks = models.DecimalField(max_digits=11, decimal_places=5, blank=True, null=True)
-    is_practice = models.BooleanField(default=False)
     date_student_started = models.DateTimeField()
     date_student_finished = models.DateTimeField()
-    
+
     def __str__(self):
         return f"""
             {self.student.username=}
