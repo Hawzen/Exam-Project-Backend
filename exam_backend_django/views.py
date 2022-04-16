ff500519158ff35cfdcdf7e02a9d7febe33bdcae
Hawzen <hawzen@users.noreply.github.com>
2022-04-16 09:29:55 +0300

    Implement get_ranking_view and add exception handler decorator

--- exam_backend_django/views.py
+++ exam_backend_django/views.py
@@ -2,11 +2,11 @@ import json
 from functools import wraps
 
 from django.contrib.auth import authenticate, login, logout
-from django.http import JsonResponse, HttpResponse
+from django.db import IntegrityError
+from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from django.views.decorators.http import require_http_methods
 from django.contrib.auth.models import User
-from django.forms.models import model_to_dict
 
 from . import models
 
@@ -29,9 +29,18 @@ def login_required(function):
         return function(request, *args, **kwargs)
     return wrapper
 
+def exception_catcher(function):
+    def wrapper(request, *args, **kwargs):
+        try:
+            return function(request, *args, **kwargs)
+        except IntegrityError as e:
+            return JsonResponse({"status":"failed", "message": f"database exception {e.message}."}, status=400)
+    return wrapper
+
 # Views
 
 @csrf_exempt
+@exception_catcher
 @require_http_methods(["POST"])
 @require_params("student_id", "password", "nickname")
 def register_view(request):
@@ -41,6 +50,7 @@ def register_view(request):
     return JsonResponse({"message": "success"}, status=200)
     
 @csrf_exempt
+@exception_catcher
 @require_http_methods(["POST"])
 @require_params("student_id", "password")
 def login_view(request):
@@ -53,6 +63,7 @@ def login_view(request):
         return JsonResponse({"status":"failed", "message": "Incorrect login details"}, status=401)
 
 @csrf_exempt
+@exception_catcher
 @login_required
 @require_http_methods(["POST"])
 def logout_view(request):
@@ -60,6 +71,7 @@ def logout_view(request):
     return JsonResponse({"message": "success"}, status=200)
 
 @csrf_exempt
+@exception_catcher
 @login_required
 @require_http_methods(["POST"])
 @require_params("start_index", "end_index")
@@ -79,3 +91,22 @@ def get_exams_view(request):
     ]
     exams = models.Exam.objects.order_by("-date_registered")[data["start_index"]:data["end_index"]].values(*fields)
     return JsonResponse({"exams": list(exams), "message": "success"}, status=200)
+
+@csrf_exempt
+@exception_catcher
+@login_required
+@require_http_methods(["POST"])
+@require_params("exam_name")
+def get_ranking_view(request):
+    MAX_EXAMS_PER_REQUEST = 100
+    data = json.loads(request.body)
+    if MAX_EXAMS_PER_REQUEST < data["end_index"] - data["start_index"]:
+        return JsonResponse({"status":"failed", "message": "wrong parameters, or wrong format."}, status=400)
+    exam = models.Exam.objects.get(exam_name=data["exam"])
+    ranking = models.Student_on_Exam.objects.filter(
+        exam=exam
+        ).select_related("student") 
+    ranking = [
+        {"nickname": attempt.student.nickname, "marks": attempt.student_marks} for attempt in ranking
+    ]
+    return JsonResponse({"ranking": ranking, "message": "success"}, status=200)
